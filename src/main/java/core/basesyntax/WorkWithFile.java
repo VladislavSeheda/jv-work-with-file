b43a5a04972703d80792fb073d425cedfb04756a
Владіслав <vladislav.seheda@gmail.com>
2025-08-20 13:01:22 +0300

    qq

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,9 +1,9 @@
 package core.basesyntax;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
